6d45571a344fbeab32fa206a168b45ca86514697
shreepad sawant <shreesawant83@gmail.com>
2024-07-13 17:58:56 +0530

    Day 1 task completed

---
+++ Day1/index.js
@@ -0,0 +1,45 @@
+//Start Learning
+
+//Activity 1 Variable declaration
+var taskOne=1;
+console.log(taskOne);
+
+let taskTwo = "Developer";
+console.log(taskTwo)
+
+//Activity 2 Constant Declaration
+const taskThree= true;
+console.log(taskThree)
+
+//Activity 3 Data types 
+
+let numValue=88;
+let stringValue="I am Developer";
+let boolValue=true;
+let objValue={
+    name:"Shreepad",
+    profile:"Developer"
+}
+let arrValue = ["Developer","Tester","Consultant"];
+
+console.log(typeof numValue);
+console.log(typeof stringValue);
+console.log(typeof boolValue);
+console.log(typeof objValue);
+console.log(typeof arrValue);
+
+//Activity 4 Reassigning values
+
+let assignCheckValue="Js Developer"
+console.log(assignCheckValue);
+
+assignCheckValue="React Developer";
+console.log(assignCheckValue);
+
+//Activity 5 Understanding Const
+
+const understandingConstValue=true;
+console.log(understandingConstValue);
+
+understandingConstValue=false;
+console.log(understandingConstValue);
